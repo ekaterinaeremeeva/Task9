a695d42159324c7c4c5b7e22b79e38ec5dc2b168
Ekaterina <perovakatharina@gmail.com>
2023-11-08 19:59:26 +0300

    Решена задача №3

--- js/1.js
+++ js/1.js
@@ -42,3 +42,34 @@ function task2()
 
     orders.forEach(elem=>showOrder.call(elem));
 }
+
+function task3()
+{
+    let orders=[
+        {
+            fio:"Ivan",
+            tovar:"phone",
+            price:10000,
+            quantity:2
+        },
+        {
+            fio:"Peter",
+            tovar:"usb",
+            price:2300,
+            quantity:5
+        },
+        {
+            fio:"Mary",
+            tovar:"connector",
+            price:100,
+            quantity:100
+        },
+    ]
+    function showOrder(action){
+        let str=`Заказ покупателя ${this.fio} составил ${this.price*this.quantity*(100-action)/100}, скидка составила ${action}%`;
+        console.log(str);
+    }
+
+    orders.forEach(elem=>showOrder.call(elem,0));
+    orders.forEach(elem=>showOrder.call(elem,10));
+}
