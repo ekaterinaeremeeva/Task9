a5cbdcf24924a470e88745bc030756ccebabb4bc
Ekaterina <perovakatharina@gmail.com>
2023-11-08 19:40:02 +0300

    Решена задача №2

--- js/1.js
+++ js/1.js
@@ -10,3 +10,33 @@ function task1()
     createNotebooksLink = createLinkTovar('notebooks');
     console.log(createPCLink(1001));
 }
+
+function task2()
+{
+    let orders=[
+        {
+            fio:"Ivan",
+            tovar:"phone",
+            price:10000,
+            quantity:2
+        },
+        {
+            fio:"Peter",
+            tovar:"usb",
+            price:2300,
+            quantity:5
+        },
+        {
+            fio:"Mary",
+            tovar:"connector",
+            price:100,
+            quantity:100
+        },
+    ]
+    function showOrder(){
+        let str=`Заказ покупателя ${this.fio} составил ${this.price*this.quantity}`;
+        console.log(str);
+    }
+
+    orders.forEach(elem=>showOrder.call(elem));
+}
