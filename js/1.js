187eccebc6f55a390c2451894f29dabfbb761327
Ekaterina <perovakatharina@gmail.com>
2023-11-08 19:19:48 +0300

    Решена задача №1

---
+++ js/1.js
@@ -0,0 +1,12 @@
+function task1()
+{
+    function createLinkTovar(category){
+        return function(artikul) {
+            return `https://myshop.ru/${category}/${artikul}`;
+        }
+    }
+    createPCLink = createLinkTovar('pc');
+    createMonitorLink = createLinkTovar('monitors');
+    createNotebooksLink = createLinkTovar('notebooks');
+    console.log(createPCLink);
+}
